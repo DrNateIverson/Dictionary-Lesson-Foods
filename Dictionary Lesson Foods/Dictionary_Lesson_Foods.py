4dd260a2a82de2aae9496de3b46c817f0b58918a
Nathaniel Iverson <niverson@sienaheights.edu>
2023-02-07 15:02:01 -0500

    Made list of dictionaries for multiple values. Mimics database structure

--- Dictionary Lesson Foods/Dictionary_Lesson_Foods.py
+++ Dictionary Lesson Foods/Dictionary_Lesson_Foods.py
@@ -1,12 +1,17 @@
-name_list=['banana','egg','cheese pizza','tamales']
+#name_list=['banana','egg','cheese pizza','tamales']
 #vegan=[True,False,False,True]
 #calories=[105,78, 213, 284]
 
-vegan_dict={'banana':True, 'egg':False, 'cheese pizza':False, 'tamales':True  }
+#vegan_dict={'banana':True, 'egg':False, 'cheese pizza':False, 'tamales':True  }
 
-for name in name_list:
-    if vegan_dict[name]:
-        print(f"{name} is vegan")
+foods=[{'name':'banana','vegan':True, 'calories':105},
+       {'name':'egg','vegan':False, 'calories':78},
+       {'name':'cheese pizza','vegan':False, 'calories':213},
+       {'name':'tamales','vegan':True, 'calories':284}]
+
+for food in foods:
+    if food['vegan']:
+        print(f"{food['name']} is vegan and has {food['calories']} calories ")
     else:
-        print(f"{name} is not vegan")
+        print(f"{food['name']} is not vegan and has {food['calories']} calories ")
 
